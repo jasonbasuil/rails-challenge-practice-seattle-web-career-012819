a2ef5602e3e867a2a368d2f80aec4d02bcb9cf8c
jasonbasuil <basuilj@gmail.com>
2019-02-28 17:30:05 -0800

    Still working

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,2 +1,22 @@
 class BuildingsController < ApplicationController
+
+  def index
+    @buildings = Building.all
+  end
+
+  def show
+    @building = Building.find(params[:id])
+    byebug
+  end
+
+  def edit
+
+  end
+
+  private
+
+  def building_params
+    params.require(:building).permit(:name, :country, :address, :rent_per_floor, :number_of_floors)
+  end
+
 end
